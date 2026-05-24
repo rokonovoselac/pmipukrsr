21c9d2bf16ecad3390af3497fe1f6440d0d005b7
rnovosela23 <rnovosela23@unizd.hr>
2026-05-24 17:02:54 +0000

    Dodan /hostname endpoint i GREETING env varijabla

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,6 +1,10 @@
 
 package com.example.demo;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,9 +21,18 @@ public class DemoApplication {
     @RestController
     class HelloWorldController {
 
+        @Value("${greeting:Hello}")
+        private String greeting;
+
         @GetMapping("/hello/{name}")
         public String hello(@PathVariable String name) {
             return "Hello " + name + "!\n";
+        }
+
+        @GetMapping("/hostname")
+        public String hostname() throws UnknownHostException {
+            String hn = InetAddress.getLocalHost().getHostName();
+            return greeting + " from " + hn + "\n";
         }
     }
 }
